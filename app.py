def39c70cc3fac94c2b9d53d61c760c50163ca99
prabhaspp <indra.lochans@gmail.com>
2023-03-17 22:24:55 +0530

    changes

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ def writeFile(string):
 
 def readFile():
     f = open('userid.txt', 'r')
-    return int(f.readline())
+    v=f.readline()
+    return int(v)
 
 def ClearFile():
     f= open('userid.txt','w')
@@ -45,11 +46,7 @@ def landing():
 @app.route('/notes')
 def notes():
     userId=readFile()
-<<<<<<< HEAD
-    notes=Note.query.filter_by(user_id=userId).first()
-=======
     notes=Note.query.filter_by(user_id=userId).all()
->>>>>>> 3f490b54df917c1255447fa66c69de5fe40cc6fe
     return render_template('notes.html',note=notes)
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -105,11 +102,6 @@ def update(id):
         db.session.commit()
         return redirect('/notes')
     return render_template('update.html', note=note)
-# @app.route('/notes/<int:id>', methods=['GET', 'POST'])
-# def oneNote(id):
-#     note=Note.query.get(id)
-#     myNote=note.content
-#     return redirect('/notes', note=myNote)
 @app.route('/delete/<int:id>')
 def delete(id):
     note = Note.query.get(id)
